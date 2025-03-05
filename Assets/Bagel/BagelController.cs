8fd14a266a196c163fdfa0a4eac404436f0571bb
damian <damian@unity3d.com>
2025-03-05 15:22:50 -0500

    Added BagelTracker prefab

--- Assets/Bagel/BagelController.cs
+++ Assets/Bagel/BagelController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BagelController : MonoBehaviour
 {
+    public float rollTorque = 20.0f; // Torque applied for rolling
+    public float tiltRecoverySpeed = 5.0f; // Speed at which it recovers its upright tilt
 
     Rigidbody rb;
 
@@ -13,16 +15,25 @@ public class BagelController : MonoBehaviour
     void Update()
     {
         // Get current orientation:
-        var currentOrientation = transform.forward.normalized;
+        //var currentOrientation = transform.forward.normalized;
 
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce(currentOrientation * 1);
+            rb.AddTorque(Vector3.right * rollTorque, ForceMode.Force);
         if (Input.GetKey(KeyCode.S))
-            rb.AddForce(currentOrientation * -1);
+            rb.AddTorque(Vector3.right * -rollTorque, ForceMode.Force);
         if (Input.GetKey(KeyCode.A))
-            rb.AddTorque(Vector3.up * 1);
+            rb.AddTorque(Vector3.up * rollTorque, ForceMode.Force);
         if (Input.GetKey(KeyCode.D))
-            rb.AddTorque(Vector3.down * 1);
+            rb.AddTorque(Vector3.down * rollTorque, ForceMode.Force);
 
+        TiltUpright();
+    }
+
+    void TiltUpright()
+    {
+        Vector3 currentUp = transform.up;
+        Vector3 targetUp = Vector3.up; // Always align with world's "up" direction
+        Vector3 tiltCorrectionTorque = Vector3.Cross(currentUp, targetUp) * tiltRecoverySpeed;
+        rb.AddTorque(tiltCorrectionTorque, ForceMode.Force);
     }
 }
