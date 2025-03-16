edcd2da6ff4d15ef843642f081e2eb65c2fcc73b
damian <damian@unity3d.com>
2025-03-16 14:38:11 -0400

    Fixed tracker

--- Assets/Bagel/BagelTracker.cs
+++ Assets/Bagel/BagelTracker.cs
@@ -5,14 +5,15 @@ public class BagelTracker : MonoBehaviour
 {
     public BagelController bagelController;
 
-    private void Awake()
-    {
-        
-    }
-
     private void Update()
     {
         transform.position = bagelController.transform.position;
-        transform.rotation = Quaternion.Euler( 0f, bagelController.transform.rotation.eulerAngles.y, 0f );
+
+        var right = bagelController.transform.right;
+        right.y = 0f;
+        right.Normalize();
+
+        Vector3 forward = Quaternion.AngleAxis(-90f, Vector3.up) * right;
+        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
     }
 }
