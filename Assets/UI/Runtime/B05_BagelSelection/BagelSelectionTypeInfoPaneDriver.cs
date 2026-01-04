ef2f88cb8cac6a485d1c12d2046a025fc5ec4317
damian <damian@unity3d.com>
2026-01-04 17:09:50 -0500

    Migrated BagelSelectionPodium to PanelRenderer

--- Assets/UI/Runtime/B05_BagelSelection/BagelSelectionTypeInfoPaneDriver.cs
+++ Assets/UI/Runtime/B05_BagelSelection/BagelSelectionTypeInfoPaneDriver.cs
@@ -4,24 +4,32 @@ using UnityEngine.UIElements;
 namespace Bagel
 {
     [ExecuteInEditMode]
-    [RequireComponent(typeof(UIDocument))]
+    [RequireComponent(typeof(PanelRenderer))]
     public class BagelSelectionTypeInfoPaneDriver : MonoBehaviour
     {
         [SerializeField] BagelSelectionPodium m_BagelSelectionPodium;
 
         [SerializeField] BagelType m_BagelType;
-        UIDocument m_UIDocument;
         VisualElement m_Pane;
 
         void OnEnable()
         {
-            m_UIDocument = GetComponent<UIDocument>();
-            m_Pane = m_UIDocument.rootVisualElement.Q("pane");
-            m_Pane.dataSource = null;
-
+            GetComponent<PanelRenderer>().RegisterUIReloadCallback(OnUIReload);
             m_BagelSelectionPodium.onBagelTypeChange += BagelSelectionPodium_OnBagelTypeChange;
         }
 
+        void OnDisable()
+        {
+            GetComponent<PanelRenderer>().UnregisterUIReloadCallback(OnUIReload);
+            m_BagelSelectionPodium.onBagelTypeChange -= BagelSelectionPodium_OnBagelTypeChange;
+        }
+
+        void OnUIReload(PanelRenderer panelRenderer, VisualElement rootElement)
+        {
+            m_Pane = rootElement.Q<VisualElement>("pane");
+            m_Pane.dataSource = null;
+        }
+            
         void BagelSelectionPodium_OnBagelTypeChange(object sender, BagelType bagelType)
         {
             m_BagelType = bagelType;
