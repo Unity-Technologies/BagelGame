067945d2097976863a45d90670c8a8c129c41503
damian <damian@unity3d.com>
2026-02-21 00:11:31 -0500

    Migrated BagelSelectionPodium to OnUIReload pattern

--- Assets/UI/Runtime/B05_BagelSelection/BagelSelectionTypeInfoPaneDriver.cs
+++ Assets/UI/Runtime/B05_BagelSelection/BagelSelectionTypeInfoPaneDriver.cs
@@ -10,18 +10,25 @@ namespace Bagel
         [SerializeField] BagelSelectionPodium m_BagelSelectionPodium;
 
         [SerializeField] BagelType m_BagelType;
-        UIDocument m_UIDocument;
         VisualElement m_Pane;
 
         void OnEnable()
         {
-            m_UIDocument = GetComponent<UIDocument>();
-            m_Pane = m_UIDocument.rootVisualElement.Q("pane");
-            m_Pane.dataSource = null;
-
+            OnUIReload(GetComponent<UIDocument>().rootVisualElement);
             m_BagelSelectionPodium.onBagelTypeChange += BagelSelectionPodium_OnBagelTypeChange;
         }
 
+        void OnDisable()
+        {
+            m_BagelSelectionPodium.onBagelTypeChange -= BagelSelectionPodium_OnBagelTypeChange;
+        }
+
+        void OnUIReload(VisualElement rootElement)
+        {
+            m_Pane = rootElement.Q<VisualElement>("pane");
+            m_Pane.dataSource = null;
+        }
+            
         void BagelSelectionPodium_OnBagelTypeChange(object sender, BagelType bagelType)
         {
             m_BagelType = bagelType;
